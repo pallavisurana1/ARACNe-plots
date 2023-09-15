582ff4a43350cde27112c9a00861f7d98e3374a8
Pallavi Surana <pallavi.surana.97@gmail.com>
2023-09-15 16:40:26 -0400

    newtwork plot updated

--- README.md
+++ README.md
@@ -1,6 +1,54 @@
-# ARACNe-plots
-Make network plots from ARACNe output.
+# ARACNe Network Plots
 
-Constructing a simple network plot for ARACNe output from genes of interest using some basic fun packages of R.
+This repository contains an R script for constructing network plots based on ARACNe output files. ARACNe (Algorithm for the Reconstruction of Accurate Cellular Networks) is an information-theoretic algorithm designed to infer gene regulatory networks.
 
-Reference - Margolin, A.A., Nemenman, I., Basso, K. et al. ARACNE: An Algorithm for the Reconstruction of Gene Regulatory Networks in a Mammalian Cellular Context. BMC Bioinformatics 7, S7 (2006). https://doi.org/10.1186/1471-2105-7-S1-S7
+## Reference
+
+Margolin, A.A., Nemenman, I., Basso, K. et al. ARACNe: An Algorithm for the Reconstruction of Gene Regulatory Networks in a Mammalian Cellular Context. BMC Bioinformatics 7, S7 (2006). [DOI: 10.1186/1471-2105-7-S1-S7](https://doi.org/10.1186/1471-2105-7-S1-S7)
+
+## Dependencies
+
+The following R packages are required for running the script:
+
+- `BiocManager`
+- `reshape2`
+- `EnsDb.Hsapiens.v86`
+- `dplyr`
+- `tidyverse`
+- `igraph`
+- `networkD3`
+- `magrittr`
+- `linkcomm`
+
+You can install these packages using the `pacman` package manager for R.
+
+## Instructions
+
+### 1. Data Import
+
+The script reads ARACNe output and a list of genes of interest from specified file paths.
+
+### 2. Annotation
+
+It maps Ensembl IDs for regulators and targets to their corresponding gene symbols using the `EnsDb.Hsapiens.v86` package.
+
+### 3. Data Processing
+
+The script subsets the data to focus only on genes of interest and then processes it to prepare for different levels of the network.
+
+### 4. Network Construction
+
+It uses the `linkcomm` and `networkD3` libraries to generate network plots for different hierarchical levels of the gene network.
+
+### Helper Functions
+
+The `process_data_level()` function is introduced to make the code reusable and more readable by handling data manipulation at various levels.
+
+## Running the Code
+
+To execute the script, simply source it in your R environment after installing the required dependencies and setting the file paths appropriately for the ARACNe output and genes of interest.
+
+## Plotting
+
+The script includes functionality to plot the networks both as static PDFs and as interactive HTML files.
+
--- aracne_viz.R
+++ aracne_viz.R
@@ -1,101 +1,57 @@
 
-#---https://christophergandrud.github.io/networkD3/
+## Reference - based on https://christophergandrud.github.io/networkD3/
 
-#---get packages
+# Load packages
 if (!require("pacman")) install.packages("pacman")
 pacman::p_load(BiocManager, reshape2, EnsDb.Hsapiens.v86, dplyr, tidyverse, igraph, networkD3, magrittr, linkcomm)
 
-#---get ARACNe data to make network
-df = read.csv("/path/to/network/file/network.txt", sep="")
+# Helper Functions
+process_data_level <- function(data, new_data){
+  new_data$Target = data$Regulator
+  new_data$Regulator = new_data$MI = NULL
+  new_data = merge(new_data, data, by.x="Target", by.y="Target")
+  new_data = new_data[!duplicated(new_data[1:2]), ]
+  return(new_data[, c(2, 1, 3)])
+}
 
-#---read genes of interest
-gene = read.csv("/path/to/network/file/gene_of_interest.txt", sep="")
+# Import Data
+df <- read.csv("/path/to/network/file/network.txt", sep="")
+gene <- read.csv("/path/to/network/file/gene_of_interest.txt", sep="")
 
-#---annotate the ARACNe output
+# Annotation
 Regulator_geneID <- ensembldb::select(EnsDb.Hsapiens.v86, keys= c(df$Regulator, df$Target), 
                                       keytype = "GENEID", columns = c("SYMBOL","GENEID"))
 
-#---map ids
+# Mapping IDs
 map <- setNames(Regulator_geneID$SYMBOL, Regulator_geneID$GENEID)
-x = apply(df, 2, function(x) map[as.character(x)])
-df1 = cbind(x, df)
+df1 <- cbind(apply(df, 2, function(x) map[as.character(x)]), df)
+df1 <- df1[, -c(3:6, 8)]
 
-#---subset data
-df1 = df1[ -c(3:6, 8)]
+# Subset Genes of Interest
+df_s_l1 <- subset(df1, df1$Target %in% gene$Symbol)
+df_s_l1 <- df_s_l1[order(df_s_l1$Target), ]
 
-#---subset genes of interest for level 1
-df_s_l1 = subset(df1, df1$Target %in% spingo$Symbol)
-df_s_l1 = df_s_l1[order(df_s_l1$Target), ]
+# Data Manipulation
+df_s_l2 <- process_data_level(df_s_l1, df_s_l1)
+df_s_l3 <- process_data_level(df_s_l2, df_s_l2)
+df_s_l4 <- process_data_level(df_s_l3, df_s_l3)
 
-#----- Level 2 ------# Regulator becomes Target
-df_s_l2 = df_s_l1
-df_s_l2$Target = df_s_l1$Regulator
-df_s_l2$Regulator = df_s_l2$MI = NULL
-df_s_l2$Target <- df_s_l2[order(df_s_l2$Target),]
+# Join all levels
+df2 <- bind_rows(df_s_l1, df_s_l2, df_s_l3, df_s_l4)
+df2 <- df2[!duplicated(df2[1:2]), ]
 
-#---find targets for the regulators
-df_s_l2 = merge(df_s_l2, df1, by.x="Target", by.y="Target")
-# t = subset(t, t$Target %in% spingo$Symbol)
-df_s_l2 = df_s_l2[!duplicated(df_s_l2[1:2]), ]
-df_s_l2 = df_s_l2[, c(2,1,3)]
+# Plot Levels 1 and 2
+df.level2 <- bind_rows(df_s_l1, df_s_l2)
+df.level2 <- df.level2[!duplicated(df.level2[1:2]), ]
 
-
-#----- Level 3 ------# Regulator becomes Target
-df_s_l3 = df_s_l2
-df_s_l3$Target = df_s_l2$Regulator
-df_s_l3$Regulator = df_s_l3$MI = NULL
-df_s_l3$Target <- df_s_l3[order(df_s_l3$Target),]
-
-#---find targets for the regulators
-df_s_l3 = merge(df_s_l3, df1, by.x="Target", by.y="Target")
-df_s_l3 = df_s_l3[!duplicated(df_s_l3[1:2]), ]
-df_s_l3 = df_s_l3[, c(2,1,3)]
-
-
-#----- Level 4 ------# Regulator becomes Target
-df_s_l4 = df_s_l3
-df_s_l4$Target = df_s_l3$Regulator
-df_s_l4$Regulator = df_s_l4$MI = NULL
-df_s_l4$Target <- df_s_l4[order(df_s_l4$Target),]
-
-# find targets for the regulators
-df_s_l4 = merge(df_s_l4, df1, by.x="Target", by.y="Target")
-df_s_l4 = df_s_l4[!duplicated(df_s_l4[1:2]), ]
-df_s_l4 = df_s_l4[, c(2,1,3)]
-
-
-#---Join
-#---join all 4 levels
-df2 = bind_rows(df_s_l1, df_s_l2, df_s_l3, df_s_l4)
-df2 = df2[!duplicated(df2[1:2]), ]
-
-# write.csv2(df2, "path/to/processed/data/prcoessed_network.txt", row.names = FALSE)
-
-
-#---plot only levels 1 and 2
-#---join ------ level 1 and 2 
-df.level2 = bind_rows(df_s_l1, df_s_l2)
-df.level2 = df.level2[!duplicated(df.level2[1:2]), ]
-
-
-#---plot - extracts link communities from networks
+# Plot Networks
 lm <- getLinkCommunities(df.level2[1:2])
-
-g = plot(lm, type = "graph", layout = "spencer.circle")
+g <- plot(lm, type = "graph", layout = "spencer.circle")
 
 pdf("my_plot.pdf")
-g
-dev.off() 
+print(g)
+dev.off()
 
-htmlwidgets::saveWidget(ggplotly(g), "/path/to/network/file/network.html")
-
-
-#----plot interactive simple network
-df.level2.1 = subset(df.level2, df.level2$MI>0.5)
+# Interactive Plot
+df.level2.1 <- subset(df.level2, df.level2$MI > 0.5)
 simpleNetwork(df.level2.1) %>% saveNetwork(file = '/path/to/network/file/network.html')
-
-
-#------------ END ---------------#
-
-
-
